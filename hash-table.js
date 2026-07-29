64696042de899ded3473150a054406b85399f682
j-goodman <johncurtisgoodman@gmail.com>
2026-07-28 20:50:10 -0400

    Add hash table.

---
+++ hash-table.js
@@ -0,0 +1,13 @@
+// This is a list of phone numbers arranged in key-value pairs.
+
+contacts = {
+    "Ada Lovelace": "555-2468",
+    "Charles Babbage": "555-4242",
+    "Alan Turing": "555-3141",
+    "Katherine Johnson": "555-2718",
+    "Grace Hopper": "555-9753",
+}
+
+// 1. Add. Write a function that adds someone to the table.
+// 2. Delete. Write a function that deletes someone from the table.
+// 3. Name lookup. Write a function that takes in a phone number and returns the name of the person whose number it is, or null if the number is not on the list.
